ef7c90c96e6fdb6e863b917be43846c4b8b0be1e
1754007050 <dlita@tecnologicoismac.edu.ec>
2026-05-30 09:20:33 +0700

    implementacion capa serivicio

--- src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
+++ src/test/java/com/distribuida/dao/ClienteTestIntegracion.java
@@ -12,15 +12,13 @@ import org.springframework.test.annotation.Rollback;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Transactional
 @Rollback(value = false)
-
 public class ClienteTestIntegracion {
 
 
@@ -48,13 +46,40 @@ public class ClienteTestIntegracion {
     public void save( ){
         Cliente cliente = new Cliente(
                 0,"1701234566","Juan","Taipe","Av.por ahi"
-                ,"09876543216","jtaipe@correo.com"
-        );
-
-
+                ,"09876543216","jtaipe@correo.com");
+        Cliente clienteGuardado = clienteDao.save(cliente);
+        assertNotNull(clienteGuardado.getIdCliente(),"El cliente guardado debe tener un id");
+        assertEquals("1701234566",clienteGuardado.getCedula());
+        assertEquals("Juan",clienteGuardado.getNombre());
 
     }
 
+    @Test
+    public void update(){
+        Optional<Cliente>cliente = clienteDao.findById(46);
+        assertTrue(cliente.isPresent(),"El cliente con id=44 debe existir para ser actualizado");
 
+        cliente.orElse(null).setCedula("1701233334");
+        cliente.orElse(null).setNombre("Juan2");
+        cliente.orElse(null).setApellido("Taipe2");
+        cliente.orElse(null).setDireccion("Av.33-2");
+        cliente.orElse(null).setTelefono("0978654333");
+        cliente.orElse(null).setCorreo("jtaipe33@correo.com-2");
+
+        Cliente clienteActualizado = clienteDao.save(cliente.orElse(null));
+
+        assertEquals("Juan2",clienteActualizado.getNombre());
+        assertEquals("Taipe2",clienteActualizado.getApellido());
+
+    }
+
+    @Test
+    public void delete(){
+        if(clienteDao.existsById(46)){
+            clienteDao.deleteById(46);
+        }
+        assertFalse(clienteDao.existsById(46),"El id=44 deberia haberse eliminado");
+
+    }
 
 }
